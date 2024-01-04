36e24b159b9ed1617a6f4ee5d9884baec1e82365
Rahul-NITD <211220042@nitdelhi.ac.in>
2024-01-04 22:07:47 +0530

    Test done

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/{subdomain:[a-z]+}", subdomainHandler)
+	router.HandleFunc("/", defaultHandler)
+
+	http.Handle("/", router)
+	http.ListenAndServe(":5000", nil)
+}
+
+func subdomainHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	subdomain := vars["subdomain"]
+	fmt.Fprintf(w, "Subdomain: %s, Path: %s", subdomain, r.URL.Path)
+}
+
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "No subdomain, Path: ", r.URL.Path)
+}
